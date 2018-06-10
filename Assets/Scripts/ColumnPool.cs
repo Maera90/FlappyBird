e1bc5800cfa5acfb02c695a4f8768ac6f723a27e
Maera90 <maera-90@hotmail.com>
2018-06-10 19:45:22 +0200

    FlappyBird Initial Commit

---
+++ Assets/Scripts/ColumnPool.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnPool : MonoBehaviour
+{
+    public int columnPoolSize = 5;
+    public GameObject columnPrefab;
+    public float SpawnRate = 4f;
+    public float columnMin = -1f;
+    public float columnMax = 3.5f;
+
+    private GameObject[] colums;
+    private Vector2 objectPoolPosition = new Vector2(-15f,-25f);
+    private float spawnXPosition = 10f;
+    private int currentColumn = 0;
+
+    private float timeSinceLastSpawnd;
+	// Use this for initialization
+	void Start () {
+
+		colums = new GameObject[columnPoolSize];
+
+	    for (int i = 0; i < columnPoolSize; i++)
+	    {
+	        colums[i] = Instantiate(columnPrefab, objectPoolPosition, Quaternion.identity);
+	    }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    timeSinceLastSpawnd += Time.deltaTime;
+
+	    if (GameControl.instance.gameOver == false && timeSinceLastSpawnd >= SpawnRate)
+	    {
+	        timeSinceLastSpawnd = 0;
+	        float spawmYPosition = Random.Range(columnMin, columnMax);
+	        colums[currentColumn].transform.position = new Vector2(spawnXPosition, spawmYPosition);
+	        currentColumn++;
+	        if (currentColumn >= columnPoolSize)
+	        {
+	            currentColumn = 0;
+	        }
+	    }
+	}
+}
---
+++ Assets/Scripts/RepeatingBackground.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour
+{
+
+    private BoxCollider2D groundColider;
+
+    private float groundHorizontalLength;
+	// Use this for initialization
+	void Start ()
+	{
+	    groundColider = GetComponent<BoxCollider2D>();
+	    groundHorizontalLength = groundColider.size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (transform.position.x < -groundHorizontalLength)
+	    {
+	        RepositionBackground();
+	    }
+	}
+
+    private void RepositionBackground()
+    {
+        Vector2 groundOffset = new Vector2((groundHorizontalLength * 2f)-0.5f,0);
+        transform.position = (Vector2) transform.position + groundOffset;
+    }
+}
